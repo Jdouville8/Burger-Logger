daf074404e01a5254bfbec7e5991cd0e23f0ba6e
Justin Douville <jdouville8@gmail.com>
2021-03-01 01:04:14 -0500

    populated table with seed data

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,15 @@
 DROP DATABASE IF EXISTS burgers_db;
 
 -- Create burgers_db
+CREATE DATABASE burgers_db;
 -- USE burger_db
+USE burgers_db;
 -- Create burgers table with field: id (Int), burger_name (string), devoured (boolean)
+CREATE TABLE burgers (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
 -- Insert at least 3 entries to populate table
+INSERT INTO burgers (burger_name, devoured) VALUES ('Texas heater', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger deluxe', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Iceburger', false);
+
+SELECT * FROM burgers;
